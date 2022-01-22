493ae9f6f128a7c8d3241cf6684c080e748464a6
anjiraj1442 <anjiraj1442@gmail.com>
2022-01-23 02:00:20 +0530

    Added:validation is added to login form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,46 +1,107 @@
 import React, { Component } from "react";
-import '../login/Login.css'
+import "../login/Login.css";
 import { TextField } from "@material-ui/core";
 
 export class Login extends Component {
-    render() {
-        return (
-            <div className="login-main-page">
-                <div className="login-content">
-                    <div className="first-content">
-                        <div className="Fundoo">
-                        <font color="#0A5BB4"> F </font>
-                        <font color="#C11D38"> U </font>
-                        <font color="#EDF750"> N </font>
-                        <font color="#0A5BB4"> D </font>
-                        <font color="#71EFA3"> O </font>
-                        <font color="#8D1F38"> O </font> 
-                        </div>
-                        <p className="sign-in">Sign in</p>
-                        < p className="account">Use your Fundoo Account</p>
-                    </div>
-                    <div className="email">
-                        <TextField id="email" label="Email or phone" variant="outlined" helperText="Forgot Email?" fullWidth />
-                    </div>
-                    <br></br>
-                    <div className="password">
-                        <TextField id="Password" label="Password" variant="outlined" helperText="Forgot Password" fullWidth />
-
-                    </div>
-                    <div className="else-computer">
-                        <p className="text">Not your computer? Use Guest mode to sign in privately.</p>
-                        <p className="learnmore">Learn more</p>
-                    </div>
-                    <div className="create">
-                        <p className="c-text" >Create account ?</p>
-                        <div className="box">
-                            <button className="c-next">Next</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
+  constructor(props) {
+      super(props);
+      this.state = {
+        emailaddress: "",
+        emailaddressError:"",
+        passWord:"",
+        passwordError:""
+      };
     }
+    changeHandle = (e) => {
+      console.log(e.target.value);
+      this.setState({
+        [e.target.name]: e.target.value,
+      });
+    };
+    validation = () => {
+      let isError = false;
+      const error = this.state;
+      error.emailaddressError = this.state.emailaddress === "" ? true : false;
+      error.passwordError = this.state.passWord === "" ? true : false;
+      this.setState({
+        ...error,
+      });
+      return (isError = error.emailaddressError || error.passwordError);
+    };
+    next = () => {
+      var validated = this.validation();
+      if (validated) {
+        console.log("Validation Completed");
+      } else {
+        console.log("somethingmissing");
+      }
+    };
+  render() {
+    return (
+      <div className="login-main-page">
+        <div className="first-content">
+          <div className="Fundoo">
+            <font color="#0A5BB4"> F </font>
+            <font color="#C11D38"> U </font>
+            <font color="#EDF750"> N </font>
+            <font color="#0A5BB4"> D </font>
+            <font color="#71EFA3"> O </font>
+            <font color="#8D1F38"> O </font>
+          </div>
+          <h2>Sign in</h2>
+          <p className="account">Use your Fundoo Account</p>
+        </div>
+        <div className="second-content">
+          <div className="email">
+            <TextField
+              name="emailaddress"
+              id="Email"
+              label="Email or phone"
+              variant="outlined"
+              helperText="Forgot Email?"
+              size="small"
+              fullWidth
+              error={this.state.emailaddressError}
+              helperText={
+                this.state.emailaddressError ? "email or phone required" : " "
+              }
+              onChange={(e) => this.changeHandle(e)}
+            />
+          </div>
+
+          <div className="password">
+            <TextField
+            name="passWord"
+              id="Password"
+              label="Password"
+              variant="outlined"
+              helperText="Forgot Password"
+              size="small"
+              fullWidth
+              error={this.state.passwordError}
+              helperText={
+                this.state.passwordError ? "password required" : " "
+              }
+              onChange={(e) => this.changeHandle(e)}
+            />
+          </div>
+        </div>
+        <div className="third-content">
+          <h5 className="not">
+            Not your computer? Use Guest mode to sign in privately.
+          </h5>
+          <a href=""  id="more"  className="learn">Learn more</a>
+        </div>
+        <div className="fourth-content">
+          <a href="" className="learn">Create account</a>
+          <div>
+            {" "}
+            <button id="next" onClick={this.next}>Next</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
