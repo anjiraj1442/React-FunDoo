688db0e45adad2987dd7d96667daed098e164099
anjiraj1442 <anjiraj1442@gmail.com>
2022-01-22 09:51:20 +0530

    Added:validation is added

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,46 +1,54 @@
 import React, { Component } from "react";
-import '../login/Login.css'
+import "../login/Login.css";
 import { TextField } from "@material-ui/core";
+import Button from "@mui/material/Button";
 
 export class Login extends Component {
-    render() {
-        return (
-            <div className="login-main-page">
-                <div className="login-content">
-                    <div className="first-content">
-                        <div className="Fundoo">
-                        <font color="#0A5BB4"> F </font>
-                        <font color="#C11D38"> U </font>
-                        <font color="#EDF750"> N </font>
-                        <font color="#0A5BB4"> D </font>
-                        <font color="#71EFA3"> O </font>
-                        <font color="#8D1F38"> O </font> 
-                        </div>
-                        <p className="sign-in">Sign in</p>
-                        < p className="account">Use your Fundoo Account</p>
-                    </div>
-                    <div className="email">
-                        <TextField id="email" label="Email or phone" variant="outlined" helperText="Forgot Email?" fullWidth />
-                    </div>
-                    <br></br>
-                    <div className="password">
-                        <TextField id="Password" label="Password" variant="outlined" helperText="Forgot Password" fullWidth />
-
-                    </div>
-                    <div className="else-computer">
-                        <p className="text">Not your computer? Use Guest mode to sign in privately.</p>
-                        <p className="learnmore">Learn more</p>
-                    </div>
-                    <div className="create">
-                        <p className="c-text" >Create account ?</p>
-                        <div className="box">
-                            <button className="c-next">Next</button>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="Box-class">
+        <div className="form-class">
+          <div className="fundoo">
+            <font color="#0A5BB4"> F </font>
+            <font color="#C11D38"> U </font>
+            <font color="#EDF750"> N </font>
+            <font color="#0A5BB4"> D </font>
+            <font color="#71EFA3"> O </font>
+            <font color="#8D1F38"> O </font>F
+          </div>
+          <div className="sign">
+            <h2>Sign In</h2>
+          </div>
+          <div className="account-text">
+            <h5>Use Your Account Here</h5>
+          </div>
+          <div className="email1">
+            <TextField
+              id="Email"
+              label="Email or Phone"
+              size="small"
+              variant="outlined"
+            />
+          </div>
+          <div className="password">
+            <TextField label="Password" size="small" variant="outlined" />
+          </div>
+          <div className="textletter">Use to Login in privately</div>
+          <div className="account">
+            <h2 className="accountcolor">
+              <a id="GFG" href="Login.css">
+                {" "}
+                Create New Account
+              </a>
+            </h2>
+            <div className="btm">
+              <Button variant="contained">login</Button>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Login
+export default Login;
