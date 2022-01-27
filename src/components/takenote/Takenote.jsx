a306d36789eb8fcc3cd41618de9b946d62a0c63c
anjiraj1442 <anjiraj1442@gmail.com>
2022-01-27 10:58:37 +0530

    Added:app icon and search bar and nav icons is added

---
+++ src/components/takenote/Takenote.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Takenote() {
+     const [openNote,setOpenNote]= React.useState(false)
+  return <div>
+  <div>
+       <input type="text"/>
+  </div>
+
+
+  </div>;
+}
+
+export default Takenote;
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import "../dashboard/Dashboard.scss";
+import takenote from '../../components/takenote/Takenote'
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
 import MuiAppBar from "@mui/material/AppBar";
@@ -9,28 +12,22 @@ import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
-import { styled, useTheme } from "@mui/material/styles";
-
-import "../dashboard/Dashboard.scss";
-import { TextField, Button } from "@material-ui/core";
-
 import LightbulbOutlinedIcon from "@mui/icons-material/LightbulbOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
 import ArchiveOutlinedIcon from "@mui/icons-material/ArchiveOutlined";
-import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
 import RefreshOutlinedIcon from "@mui/icons-material/RefreshOutlined";
 import ViewStreamOutlinedIcon from "@mui/icons-material/ViewStreamOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import keep from '../../images/keep.png'
+
+
 
 const drawerWidth = 240;
 
@@ -61,17 +58,19 @@ const DrawerHeader = styled("div")(({ theme }) => ({
   justifyContent: "flex-end",
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
-  backgroundcolor: "#fff",
 }));
 
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
   zIndex: theme.zIndex.drawer + 1,
+  backgroundColor: "white",
+  boxShadow: "0px",
+  border: "1px solid lightgray",
+  color: "black",
   transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
-    backgroundcolor: "#fff",
   }),
   ...(open && {
     marginLeft: drawerWidth,
@@ -105,23 +104,26 @@ export default function MiniDrawer() {
   let iconlist = [
     {
       icons: <LightbulbOutlinedIcon />,
-      itext: "Notes",
+      icnText: "Notes",
     },
     {
       icons: <NotificationsOutlinedIcon />,
-      itext: "Reminders",
+      icnText: "Reminder",
     },
+
     {
-      icons: <CreateOutlinedIcon />,
-      itext: "Edit Labels",
+      icons: <EditOutlinedIcon />,
+      icnText: "Edit Labels",
     },
+
     {
       icons: <ArchiveOutlinedIcon />,
-      itext: "Archive",
+      icnText: "Archive",
     },
+
     {
-      icons: <DeleteForeverOutlinedIcon />,
-      itext: "Bin",
+      icons: <DeleteOutlinedIcon />,
+      icnText: "Bin",
     },
   ];
 
@@ -133,7 +135,7 @@ export default function MiniDrawer() {
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar position="fixed" open={open}>
-        <Toolbar>
+        <Toolbar class="headbar">
           <IconButton
             color="inherit"
             aria-label="open drawer"
@@ -142,22 +144,29 @@ export default function MiniDrawer() {
           >
             <MenuIcon />
           </IconButton>
-          <div className="mainHeader">
-            <Typography variant="h4" noWrap component="div"></Typography>
-
-            <div className="search">
-              <TextField label="Search" />
-            </div>
-
-            <nav>
-              <ul className="headerIcon">
-                <RefreshOutlinedIcon />
-                <ViewStreamOutlinedIcon />
-                <SettingsOutlinedIcon />
-                <AppsOutlinedIcon />
-                <AccountCircleOutlinedIcon />
-              </ul>
-            </nav>
+          
+          <div className="keepword">
+          <img  src={keep} alt="this is keep"></img>
+          
+          <Typography variant="h6" noWrap component="div">
+            Keep
+          </Typography>
+            
+          
+          </div>
+          <div class="search-bar">
+            <input
+              className="input-search"
+              type="text"
+              placeholder="Search"
+            ></input>
+          </div>
+          <div className="nav-right-icon">
+            <RefreshOutlinedIcon />
+            <ViewStreamOutlinedIcon />
+            <SettingsOutlinedIcon />
+            <AppsOutlinedIcon />
+            <AccountCircleOutlinedIcon />
           </div>
         </Toolbar>
       </AppBar>
@@ -166,23 +175,18 @@ export default function MiniDrawer() {
         <Divider />
         <List>
           {iconlist.map((text, index) => (
-            <ListItem button key={text.itext}>
+            <ListItem button key={text.icnText}>
               <ListItemIcon>{text.icons}</ListItemIcon>
-              <ListItemText primary={text.itext} />
+              <ListItemText primary={text.icnText} />
             </ListItem>
           ))}
         </List>
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <DrawerHeader />
-        <div>
-          <TextField
-            id="outlined-basic"
-            label="Take a note"
-            variant="outlined"
-            fullWidth
-          />
-        </div>
+        <takenote />
+        <Typography paragraph></Typography>
+        <Typography paragraph></Typography>
       </Box>
     </Box>
   );
